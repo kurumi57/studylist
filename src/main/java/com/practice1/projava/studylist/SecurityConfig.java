e4192e796e7e21b07ece0a225b1b486841273050
kurumi57 <kuru1114@icloud.com>
2026-09-03 14:29:32 +0900

    ログイン認証とログアウト機能を追加

--- src/main/java/com/practice1/projava/studylist/SecurityConfig.java
+++ src/main/java/com/practice1/projava/studylist/SecurityConfig.java
@@ -4,12 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        var user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
@@ -18,7 +37,13 @@ public class SecurityConfig {
         http
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/list",true)
+                        .defaultSuccessUrl("/list", true)
+                        .permitAll()
+                );
+
+        http
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
                         .permitAll()
                 );
 
